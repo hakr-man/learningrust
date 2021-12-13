06f0ab6ec7b05870316098ff9c5dcfa0b203f682
David Joslin <088dej03@students.olatheschools.com>
2021-12-13 13:11:59 -0600

    added files

---
+++ src/print.rs
@@ -0,0 +1,25 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print.rs file");
+    // basic formatting
+    println!("{} is from {}", "Brad", "Mass");
+    //positional arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Brad", "Mass", "code"
+    );
+    //named arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "John",
+        activity = "Baseball"
+    );
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    //placeholder for debug trait
+    println!("{:?}", (12, true, "Hello"));
+
+    //basic math
+    println!("10 + 10 = {}", 10 + 10);
+}
